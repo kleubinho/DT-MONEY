868a586a346fac1c0444debebff257f557e920aa
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-02-25 02:06:16 -0300

    search form add

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,11 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
+import { SearchForm } from "./components/SearchForm";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
@@ -9,17 +14,22 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+        <SearchForm />
         <TransactionsTable>
           <tbody>
             <tr>
               <td width={"50%"}>Desenvolvimento de site</td>
-              <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
               <td>Venda</td>
               <td>13/04/2023</td>
             </tr>
             <tr>
               <td width={"50%"}>Hamburguer</td>
-              <PriceHighLight variant="outcome">R$ 59,00</PriceHighLight>
+              <td>
+                <PriceHighLight variant="outcome">R$ 59,00</PriceHighLight>
+              </td>
               <td>Alimentação</td>
               <td>13/04/2023</td>
             </tr>
