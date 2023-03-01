d47975c0321cb84640dd2a2044ec1581d9a399f5
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-03-01 18:00:52 -0300

    Update buscando transações

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -11,31 +11,40 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query?: string) => Promise<void>;
 }
 interface TransactionsContextProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const TransactionsContext = createContext({} as TransactionContextType);
 
-export function TransactionsProvider({children}:TransactionsContextProviderProps) {
-
+export function TransactionsProvider({
+  children,
+}: TransactionsContextProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function loadTransactions() {
-    const response = await fetch("http://localhost:3000/transactions");
+  async function fetchTransactions(query?: string) {
+    const url = new URL("http://localhost:3000/transactions")
+
+    if(query) {
+      url.searchParams.append('q', query)
+    }
+
+
+    const response = await fetch(url);
     const data = await response.json();
 
     setTransactions(data);
   }
 
   useEffect(() => {
-    loadTransactions();
+    fetchTransactions();
   }, []);
 
-    return (
-        <TransactionsContext.Provider value={{transactions}}>
-            {children}
-        </TransactionsContext.Provider>
-    )
+  return (
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
